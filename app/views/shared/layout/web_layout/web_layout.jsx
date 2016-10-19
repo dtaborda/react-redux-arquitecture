089033f60687688ebed6c4da86871fbf4d826f6d
Juan Zaragoza <juan.zaragpza@teracode.com>
2016-10-19 15:49:44 -0300

    Adding gcba_header in layout_web

--- app/views/shared/layout/web_layout/web_layout.jsx
+++ app/views/shared/layout/web_layout/web_layout.jsx
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
-import BacHeader from 'app/assets/bastrap3/bac-header-2.png';
 import Footer from '../footer';
+import GcbaHeader from '../gcba_header';
 
 export default class WebLayout extends Component {
 	static get propTypes() {
@@ -12,18 +12,7 @@ export default class WebLayout extends Component {
 	render() {
 		return (
 			<div>
-				<header className="navbar navbar-primary navbar-top">
-					<div className="container">
-						<div className="row">
-							<div className="col-md-6 col-sm-6">
-								<a className="navbar-brand bac-header" href="#"/>
-							</div>
-							<div className="col-md-6 col-sm-6 text-right oculto">
-								<h5 className="sub-brand"><img src={BacHeader}/></h5>
-							</div>
-						</div>
-					</div>
-				</header>
+				<GcbaHeader />
 				<div>
 					{this.props.children}
 				</div>
