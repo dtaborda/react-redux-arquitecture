9c1f35f18246550d744e034b26243f590ad14b39
Juan Zaragoza <juan.zaragpza@teracode.com>
2016-10-20 12:44:28 -0300

    Nav: two tabs indentation

--- app/views/shared/layout/nav/nav.jsx
+++ app/views/shared/layout/nav/nav.jsx
@@ -6,53 +6,53 @@ import { Link } from 'react-router'
 
 export default class Nav extends Component {
     
-    static get propTypes() {
-        return {
-            title: PropTypes.string,
-            links: PropTypes.array
-        };
-    }
+  static get propTypes() {
+    return {
+      title: PropTypes.string,
+      links: PropTypes.array
+    };
+  }
 
-    static defaultProps = {
-        links: [
-            {
-                refTo: "/",
-                displayName: "Inicio",
-                onlyActiveOnIndex: true,
-            }
-        ],
-        title: "GCBA"
-    }
+  static defaultProps = {
+    links: [
+      {
+        refTo: "/",
+        displayName: "Inicio",
+        onlyActiveOnIndex: true,
+      }
+    ],
+    title: "GCBA"
+  }
 
-    renderLinks() {
-        return (
-          this.props.links.map(function(link,index){
-              return <li key={index}><Link to={link.refTo} activeClassName="active" onlyActiveOnIndex={link.onlyActiveOnIndex}>{link.displayName}</Link></li>;
-          })
-        )
-    }
+  renderLinks() {
+    return (
+    this.props.links.map(function(link,index){
+      return <li key={index}><Link to={link.refTo} activeClassName="active" onlyActiveOnIndex={link.onlyActiveOnIndex}>{link.displayName}</Link></li>;
+    })
+    )
+  }
 
-    render() {
-        return (
-            <nav className="navbar navbar-default" role="navigation">
-                <div className="container">
-                  <div className="row">
-                      <div className="navbar-header">
-                          <a className="navbar-brand" href="#" title={"this.props.title"}>{this.props.title}</a>
-                      </div>
-                      <div className="collapse navbar-collapse" id="main-nav">
-                          <ul className="nav navbar-nav navbar-right">
-                              {this.renderLinks()}
-                              <li className="active-BA">
-                                  <a href="#">
-                                      Entrar a <img src={baBtnImage} className="glyphicon glyphicon-ba" />
-                                  </a>
-                              </li>
-                          </ul>
-                      </div>
-                  </div>
-                </div>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="container">
+          <div className="row">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#" title={"this.props.title"}>{this.props.title}</a>
+            </div>
+            <div className="collapse navbar-collapse" id="main-nav">
+              <ul className="nav navbar-nav navbar-right">
+                {this.renderLinks()}
+                <li className="active-BA">
+                  <a href="#">
+                    Entrar a <img src={baBtnImage} className="glyphicon glyphicon-ba" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
